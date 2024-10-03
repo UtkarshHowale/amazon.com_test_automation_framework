a74b673b54fac697d654073ac87dd9e73897d23e
UtkarshHowale <utkarshhowale16@gmail.com>
2024-10-03 15:22:38 +0530

    minor changes

--- src/main/java/Base/BasePage.java
+++ src/main/java/Base/BasePage.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -14,10 +15,12 @@ import com.aventstack.extentreports.ExtentTest;
 
 import Utils.ExtentReportGenerator;
 import Utils.TestUtils;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class BasePage {
 
-	protected static ChromeDriver driver;
+	protected static WebDriver driver;
 	protected static ChromeOptions options;
 	protected static FileInputStream file;
 	protected static Properties prop;
@@ -43,7 +46,15 @@ public class BasePage {
 
 	protected static void intilizeConfiguration() {
 
-		driver = new ChromeDriver();
+		String browserName = prop.getProperty("BrowserName");
+
+		if (browserName.equalsIgnoreCase("chrome")){
+			driver = new ChromeDriver();
+		} else if (browserName.equalsIgnoreCase("firefox")) {
+			driver = new FirefoxDriver();
+		} else if (browserName.equalsIgnoreCase("edge")) {
+			driver = new EdgeDriver();
+		}
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(TestUtils.PageLoad_Timeout));
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(TestUtils.ImplicitlyWait_TimeOut));
 		driver.get(prop.getProperty("BaseUrl"));
